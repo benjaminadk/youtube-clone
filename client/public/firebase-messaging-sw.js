36e53022add68c3a54aff28c79a5c4c1c84be5b8
benjaminadk <benjaminadk@gmail.com>
2018-06-22 10:20:49 -0700

    sw

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,14 +1,14 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js')
+// importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js')
+// importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js')
 
-firebase.initializeApp({
-  messagingSenderId: '578692223559'
-})
+// firebase.initializeApp({
+//   messagingSenderId: '578692223559'
+// })
 
-const messaging = firebase.messaging()
-messaging.onMessage(payload => console.log('MESSAGE RECEIVED ', payload))
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('FIREBASE MESSAGE ', payload)
+// const messaging = firebase.messaging()
+// messaging.onMessage(payload => console.log('MESSAGE RECEIVED ', payload))
+// messaging.setBackgroundMessageHandler(function(payload) {
+//   console.log('FIREBASE MESSAGE ', payload)
 
-  return self.registration.showNotification()
+//   return self.registration.showNotification()
 })
