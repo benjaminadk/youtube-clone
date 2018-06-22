18cedab1c52b8199f26a1d573e9a97298bce6385
benjaminadk <benjaminadk@gmail.com>
2018-06-22 10:40:06 -0700

    whatever

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,8 +1,5 @@
-importScripts('/__/firebase/5.0.0/firebase-app.js')
-importScripts('/__/firebase/5.0.0/firebase-messaging.js')
-importScripts('/__/firebase/init.js')
-
-var messaging = firebase.messaging()
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js')
 
 firebase.initializeApp({
   messagingSenderId: '578692223559'
